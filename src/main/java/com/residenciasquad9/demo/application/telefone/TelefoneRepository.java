db13d6cd9cc4d5ec853a1887e128bb97b7d167e9
Carlos Campos <108171029+carloscamposb@users.noreply.github.com>
2024-11-20 09:39:36 -0300

    Delete telefone

--- src/main/java/com/residenciasquad9/demo/application/telefone/TelefoneRepository.java
+++
@@ -1,7 +0,0 @@
-
-package com.example.telefoneapi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TelefoneRepository extends JpaRepository<Telefone, Integer> {
-}
